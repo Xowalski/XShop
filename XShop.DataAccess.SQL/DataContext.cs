8d1877a90bcbbebdda56d6d9c1ceda5d3fa014ac
Konrad Kowalski <konrad.kowalski1991@o2.pl>
2021-09-24 15:04:19 +0200

    EF

---
+++ XShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XShop.Model.Models;
+
+namespace XShop.DataAccess.SQL
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Item> Products { get; set; }
+        public DbSet<ItemCategory> ProductCategories { get; set; }
+    }
+}
---
+++ XShop.DataAccess.SQL/Migrations/202109241300023_Initial.cs
@@ -0,0 +1,42 @@
+﻿namespace XShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ItemCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        CreationTime = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Items",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Category = c.String(),
+                        Image = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CreationTime = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Items");
+            DropTable("dbo.ItemCategories");
+        }
+    }
+}
